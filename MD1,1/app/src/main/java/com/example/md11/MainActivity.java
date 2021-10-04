fba9336183fa734b7f0d1381164bca021c8b2e7d
Amir <amir.shaihutd@gmail.com>
2021-10-04 07:26:09 +0300

    <MD1.1 done>

---
+++ MD1,1/app/src/main/java/com/example/md11/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.md11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
